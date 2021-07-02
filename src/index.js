abf5c58312c988ea11cbb4011d0027ec9144008c
Denis Panevin <d.panevin@gmail.com>
2021-07-02 09:03:22 +0300

    Revert "fix"
    This reverts commit 6e26f197be456e7e0674fac0d85fc0c73af724b8.

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const elem = React.createElement('div', { div: 1, children: 'Hello world!' });
-
-console.log(elem);
-
-ReactDOM.render(elem, document.querySelector('#root'));
